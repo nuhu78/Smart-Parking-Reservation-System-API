fb98fac4237f160c858d27ab941f31ad796deb11
nuhu78 <22-48664-3@student.aiub.edu>
2026-04-14 10:42:00 +0600

    Initial NestJS setup

---
+++ smart-parking-api/src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [ConfigModule.forRoot({ isGlobal: true }),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '123',
+      database: 'smart_parking',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
